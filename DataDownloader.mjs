6063ef3f6df7ad661a6767d898b6e1345b9a3e9b
David Veszelovszki <veszelovszki@gmail.com>
2021-11-08 23:19:22 +0100

    Extracted DataDownloader.

---
+++ DataDownloader.mjs
@@ -0,0 +1,31 @@
+import fs from 'fs';
+import MeteoStatInterface from './MeteoStatInterface.mjs';
+import config from './config.mjs';
+
+export default class DataDownloader {
+    constructor(meteoStatInterface) {
+        this.meteoStatInterface = meteoStatInterface || new MeteoStatInterface();
+    }
+
+    async downloadData(startYear, endYear, targetFolderRelativePath) {
+        const losiMajorCoordinates = {
+            latitude: 47.8326126,
+            longitude: 19.0728598,
+            altitudeInMeters: 160,
+        };
+
+        for(let year = startYear; year <= endYear; year++) {
+            const meteoStatApiArguments = {
+                ...losiMajorCoordinates,
+                startDateString: `${year}-09-01`,
+                endDateString: `${year}-09-30`,
+            };
+            const response = await this.meteoStatInterface.callApi(meteoStatApiArguments, config.apiKey);
+            await this.writeResultToFile(meteoStatApiArguments.startDateString, meteoStatApiArguments.endDateString, response, targetFolderRelativePath);
+        }
+    }
+
+    async writeResultToFile(startDateString, endDateString, response, folderPath) {
+        const fileName = `${folderPath}${startDateString}_${endDateString}.json`;
+        await fs.promises.writeFile(fileName, JSON.stringify(response));
+    }}
--- index.mjs
+++ index.mjs
@@ -1,26 +1,16 @@
-import fs from 'fs';
-import config from './config.mjs';
-import MeteoStatInterface from './MeteoStatInterface.mjs';
+import DataDownloader from './DataDownloader.mjs';
 
-const meteoStatInterface = new MeteoStatInterface();
-
-const losiMajorCoordinates = {
-    latitude: 47.8326126,
-    longitude: 19.0728598,
-    altitudeInMeters: 160,
-};
-
-for(let year = 1980; year <= 2019; year++) {
-    const meteoStatApiArguments = {
-        ...losiMajorCoordinates,
-        startDateString: `${year}-09-01`,
-        endDateString: `${year}-09-30`,
-    };
-    const response = await meteoStatInterface.callApi(meteoStatApiArguments, config.apiKey);
-    await writeResultToFile(meteoStatApiArguments.startDateString, meteoStatApiArguments.endDateString, response);
+const actions = {
+    step1DownloadData: 'step1DownloadData',
+    step2ParseResults: 'step2ParseResults',
 }
 
-async function writeResultToFile(startDateString, endDateString, response) {
-    const fileName = `data/raw-weather-data/${startDateString}_${endDateString}.json`;
-    await fs.promises.writeFile(fileName, JSON.stringify(response));
+/* Set action here */
+const action = actions.step2ParseResults;
+
+if (action === actions.step1DownloadData) {
+    const dataDownloader = new DataDownloader();
+    await dataDownloader.downloadData(1980, 2021, 'data/raw-weather-data/')
+} else if (action === actions.step2ParseResults) {
+
 }
