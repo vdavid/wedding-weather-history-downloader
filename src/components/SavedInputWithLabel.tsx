fc9b2306ac81bad710c7481d7d868d3e17d3af49
David Veszelovszki <veszelovszki@gmail.com>
2021-11-11 11:06:48 +0100

    - Extracted input component to clean the code and to fix ID clash.

---
+++ src/components/SavedInputWithLabel.tsx
@@ -0,0 +1,33 @@
+import React, {useEffect} from 'react';
+
+interface PropsType {
+    id: string,
+    label: string,
+    isRequired: boolean,
+    maxLength?: number;
+    value?: string;
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export default function SavedInputWithLabel(props: PropsType) {
+    const [value, setValue] = React.useState<string>('');
+
+    useEffect(() => {
+        const newValue = localStorage.getItem(props.id) || '';
+        setValue(newValue);
+        // @ts-ignore Duck typing this change event for now.
+        handleOnChange({target: {value: newValue}});
+    }, []);
+
+    return <div>
+        <label htmlFor={props.id}><strong>{props.label}</strong> ({props.isRequired ? 'required': 'optional'})</label>
+        <input type="text" id={props.id} maxLength={props.maxLength} value={value} onChange={handleOnChange}/>
+    </div>;
+
+    function handleOnChange(event: React.ChangeEvent<HTMLInputElement>) {
+        localStorage.setItem(props.id, event.target.value);
+        setValue(event.target.value);
+        props.onChange(event);
+    }
+
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,17 @@
 import type {NextPage} from 'next';
 import Head from 'next/head';
 import styles from '../../styles/Home.module.css';
-import {ChangeEvent, Dispatch, MouseEvent, SetStateAction, useEffect, useState} from 'react';
+import {MouseEvent, useState} from 'react';
+import SavedInputWithLabel from '../components/SavedInputWithLabel';
 
-type StateValueAndSetter = [string | undefined, Dispatch<SetStateAction<string | undefined>>];
 const Home: NextPage = () => {
-    const step1State: { [key: string]: StateValueAndSetter } = {
-        startYear: useState(),
-        endYear: useState(),
-        latitude: useState(),
-        longitude: useState(),
-        altitudeInMeters: useState(),
-    };
-
-    useEffect(() => {
-        step1State.startYear[1](localStorage.getItem('startYear') || undefined);
-        step1State.endYear[1](localStorage.getItem('endYear') || undefined);
-        step1State.latitude[1](localStorage.getItem('latitude') || undefined);
-        step1State.longitude[1](localStorage.getItem('longitude') || undefined);
-        step1State.altitudeInMeters[1](localStorage.getItem('altitudeInMeters') || undefined);
-    }, []);
-
+    const [step1StartYear, setStep1StartYear] = useState<string>();
+    const [step1EndYear, setStep1EndYear] = useState<string>();
+    const [step1Latitude, setStep1Latitude] = useState<string>();
+    const [step1Longitude, setStep1Longitude] = useState<string>();
+    const [step1AltitudeInMeters, setStep1AltitudeInMeters] = useState<string>();
+    const [step2StartYear, setStep2StartYear] = useState<string>();
+    const [step2EndYear, setStep2EndYear] = useState<string>();
 
     return (
         <div className={styles.container}>
@@ -36,26 +27,11 @@ const Home: NextPage = () => {
                     <h2>Step 1: Download data</h2>
                     <fieldset>
                         <div className={styles.grid}>
-                            <div>
-                                <label htmlFor="startYear"><strong>Start year</strong> (required)</label>
-                                <input type="text" id="startYear" maxLength={4} value={step1State.startYear[0]} onChange={saveData}/>
-                            </div>
-                            <div>
-                                <label htmlFor="endYear"><strong>End year</strong> (required)</label>
-                                <input type="text" id="endYear" maxLength={4} value={step1State.endYear[0]} onChange={saveData}/>
-                            </div>
-                            <div>
-                                <label htmlFor="latitude"><strong>Latitude</strong> (required)</label>
-                                <input type="text" id="latitude" step="any" value={step1State.latitude[0]} onChange={saveData}/>
-                            </div>
-                            <div>
-                                <label htmlFor="longitude"><strong>Longitude</strong> (required)</label>
-                                <input type="text" id="longitude" step="any" value={step1State.longitude[0]} onChange={saveData}/>
-                            </div>
-                            <div>
-                                <label htmlFor="altitudeInMeters"><strong>Altitude in meters</strong> (optional)</label>
-                                <input type="text" id="altitudeInMeters" maxLength={4} value={step1State.altitudeInMeters[0]} onChange={saveData}/>
-                            </div>
+                            <SavedInputWithLabel id='step1StartYear' label='Start year' isRequired={true} maxLength={4} onChange={(event) => setStep1StartYear(event.target.value)} />
+                            <SavedInputWithLabel id='step1EndYear' label='End year' isRequired={true} maxLength={4} onChange={(event) => setStep1EndYear(event.target.value)} />
+                            <SavedInputWithLabel id='step1Latitude' label='Latitude' isRequired={true} onChange={(event) => setStep1Latitude(event.target.value)} />
+                            <SavedInputWithLabel id='step1Longitude' label='Longitude' isRequired={true} onChange={(event) => setStep1Longitude(event.target.value)} />
+                            <SavedInputWithLabel id='step1AltitudeInMeters' label='Altitude in meters' isRequired={false} maxLength={4} onChange={(event) => setStep1AltitudeInMeters(event.target.value)} />
                         </div>
                         <button onClick={startDownload}>Download</button>
                     </fieldset>
@@ -65,40 +41,16 @@ const Home: NextPage = () => {
                     <h2>Step 2: Parse data</h2>
                     <fieldset>
                         <div className={styles.grid}>
-                            <div>
-                                <label htmlFor="startYear"><strong>Start year</strong> (required)</label>
-                                <input type="text" id="startYear" maxLength={4} value={step1State.startYear[0]} onChange={saveData}/>
-                            </div>
-                            <div>
-                                <label htmlFor="endYear"><strong>End year</strong> (required)</label>
-                                <input type="text" id="endYear" maxLength={4} value={step1State.endYear[0]} onChange={saveData}/>
-                            </div>
-                            <div>
-                                <label htmlFor="latitude"><strong>Latitude</strong> (required)</label>
-                                <input type="text" id="latitude" step="any" value={step1State.latitude[0]} onChange={saveData}/>
-                            </div>
-                            <div>
-                                <label htmlFor="longitude"><strong>Longitude</strong> (required)</label>
-                                <input type="text" id="longitude" step="any" value={step1State.longitude[0]} onChange={saveData}/>
-                            </div>
-                            <div>
-                                <label htmlFor="altitudeInMeters"><strong>Altitude in meters</strong> (optional)</label>
-                                <input type="text" id="altitudeInMeters" maxLength={4} value={step1State.altitudeInMeters[0]} onChange={saveData}/>
-                            </div>
+                            <SavedInputWithLabel id='step2StartYear' label='Start year' isRequired={true} maxLength={4} onChange={(event) => setStep2StartYear(event.target.value)} />
+                            <SavedInputWithLabel id='step2EndYear' label='End year' isRequired={true} maxLength={4} onChange={(event) => setStep2EndYear(event.target.value)} />
                         </div>
-                        <button onClick={startDownload}>Download</button>
+                        <button onClick={startParsing}>Parse</button>
                     </fieldset>
+                    <div id="parsingStatus"/>
                 </section>
             </main>
         </div>
     );
-
-    function saveData(event: ChangeEvent<HTMLInputElement>) {
-        const newValue = event.target.value;
-        const fieldName = event.target.getAttribute('id') || 'default';
-        localStorage.setItem(fieldName, newValue);
-        step1State[fieldName][1](newValue);
-    }
 
     async function startDownload(event: MouseEvent<HTMLButtonElement>) {
         // @ts-ignore
@@ -110,11 +62,11 @@ const Home: NextPage = () => {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                startYear: step1State.startYear[0],
-                endYear: step1State.endYear[0],
-                latitude: step1State.latitude[0],
-                longitude: step1State.longitude[0],
-                altitudeInMeters: step1State.altitudeInMeters[0],
+                startYear: step1StartYear ? parseInt(step1StartYear) : undefined,
+                endYear: step1EndYear ? parseInt(step1EndYear) : undefined,
+                latitude: step1Latitude ? parseInt(step1Latitude) : undefined,
+                longitude: step1Longitude ? parseInt(step1Longitude) : undefined,
+                altitudeInMeters: step1AltitudeInMeters ? parseInt(step1AltitudeInMeters) : undefined,
             })
         });
         if (response.ok) {
@@ -126,6 +78,35 @@ const Home: NextPage = () => {
             }
         } else {
             document.getElementById('downloadStatus')!.innerText = 'Communication error: ' + response.status;
+        }
+        // @ts-ignore
+        event.target.disabled = false;
+    }
+
+    async function startParsing(event: MouseEvent<HTMLButtonElement>) {
+        // @ts-ignore
+        event.target.disabled = true;
+        document.getElementById('parsingStatus')!.innerText = 'Parsing...';
+        const response = await fetch('/api/parse-point-hourly-data', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                startYear: step2StartYear ? parseInt(step2StartYear) : undefined,
+                endYear: step2EndYear ? parseInt(step2EndYear) : undefined,
+            })
+        });
+        if (response.ok) {
+            const responseObject = await response.json();
+            if (responseObject.success) {
+                document.getElementById('parsingStatus')!.innerText = responseObject.message;
+            } else {
+                document.getElementById('parsingStatus')!.innerText = 'Back-end error: ' + responseObject.message;
+            }
+        } else {
+            document.getElementById('parsingStatus')!.innerText = 'Communication error ' + response.status + ': ' + await response.text();
+            console.log(response);
         }
         // @ts-ignore
         event.target.disabled = false;
